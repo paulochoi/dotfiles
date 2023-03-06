0e33767bac047c9fb1264e9331298296552c4a64
Paulo Choi <paulinho@hey.com>
2023-03-06 10:05:52 -0500

    add more config

--- nvim/lua/paulinho/init.lua
+++ nvim/lua/paulinho/init.lua
@@ -1,3 +1,5 @@
 require("paulinho.remap")
 require("paulinho.set")
 vim.g.settermguicolors = true
+vim.opt.title = true
+vim.opt.titlestring = [[%f %h%m%r%w %{v:progname} (%{tabpagenr()} of %{tabpagenr('$')})]]
