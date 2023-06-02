a4bcc0ae5a6775bcafa9ba8f408760716dcf38d5
Paulo Choi <paulinho@hey.com>
2023-06-02 19:55:05 +0000

    tweak settings

--- .config/nvim/after/plugin/neoclip.lua
+++ .config/nvim/after/plugin/neoclip.lua
@@ -1,10 +1,23 @@
 require("neoclip").setup({
-  default_register = { '"', '+', '*' },
-  on_select = {
-    set_reg = true,
-    move_to_front = false,
-    close_telescope = true,
-  },
+	default_register = { '"', "+", "*" },
+	on_select = {
+		set_reg = true,
+		move_to_front = false,
+		close_telescope = true,
+	},
+	keys = {
+		telescope = {
+			i = {
+				select = "<cr>",
+				paste = "<c-l>",
+				paste_behind = "<c-k>",
+				replay = "<c-q>", -- replay a macro
+				delete = "<c-d>", -- delete an entry
+				edit = "<c-e>", -- edit an entry
+				custom = {},
+			},
+		},
+	},
 })
 
 require("telescope").load_extension("neoclip")
