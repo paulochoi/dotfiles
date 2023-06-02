0de877f1487de7bd5a74f99f0f559103c8f21a84
Paulo Choi <paulinho@hey.com>
2023-06-02 14:57:18 +0000

    tweak scrolloff

--- .config/nvim/lua/paulinho/set.lua
+++ .config/nvim/lua/paulinho/set.lua
@@ -25,7 +25,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
