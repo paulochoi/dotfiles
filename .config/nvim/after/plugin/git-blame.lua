f35ff49e5234b3149450406667752f0cf58a55cd
Paulo Choi <paulinho@hey.com>
2023-06-16 15:09:10 +0000

    adjust mapping for blame

--- .config/nvim/after/plugin/git-blame.lua
+++ .config/nvim/after/plugin/git-blame.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>b", ":GitBlameToggle<CR>")
 vim.keymap.set("n", "<leader>bo", ":GitBlameOpenCommitURL<CR>")
-vim.keymap.set("n", "<leader>gf", ":GitBlameOpenFileURL<CR>")
+vim.keymap.set("n", "<leader>fo", ":GitBlameOpenFileURL<CR>")
