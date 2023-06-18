1834fb2d36a265840c41618b5fe0d41bbf20a383
Paulo Choi <paulinho@hey.com>
2023-06-18 03:19:18 +0000

    update commits

--- .config/nvim/after/plugin/nvimtree.lua
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -40,36 +40,13 @@ function grep_at_current_tree_node()
 	require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path } })
 end
 
-
 function find_file_at_current_tree_node()
 	local node = require("nvim-tree.lib").get_node_at_cursor()
 	if not node then
 		return
 	end
-	require("telescope.builtin").find_files({ search_dirs = { node.absolute_path } })
+	require("telescope.builtin").find_files({ search_dirs = { node.absolute_path }, find_command = { "rg", "--files", "--hidden", "--no-ignore", "-L" } })
 end
 
--- open nvim-tree on setup
-local function open_nvim_tree(data)
-	-- buffer is a [No Name]
-	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-	-- buffer is a directory
-	local directory = vim.fn.isdirectory(data.file) == 1
-
-	if not no_name and not directory then
-		return
-	end
-
-	-- change to the directory
-	if directory then
-		vim.cmd.cd(data.file)
-	end
-
-	-- open the tree
-	require("nvim-tree.api").tree.open()
-end
-
--- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 vim.keymap.set("n", "<leader>rt", ":NvimTreeCollapse<CR>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
