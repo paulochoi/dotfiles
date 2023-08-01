d9c09d9e4bbda3a2834ef71e5cf700902a280936
Paulo Choi <paulinho@hey.com>
2023-08-01 00:52:44 +0000

    add navigation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,5 +7,17 @@ return {
       query = "rainbow-parens",
       strategy = require("ts-rainbow").strategy.global,
     }
+    opts.textobjects = {
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["gn"] = { query = "@function.outer", desc = "🌲go to next function" },
+        },
+        goto_previous_start = {
+          ["gp"] = { query = "@function.outer", desc = "🌲go to previous function" },
+        },
+      },
+    }
   end,
 }
